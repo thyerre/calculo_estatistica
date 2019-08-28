a0e4436d47fe5fffc2d25978182465a0b1ec4502
Thyerre Range <thyerre321@gmail.com>
2019-08-27 21:39:53 -0300

    ajustes link github

--- navBar.php
+++ navBar.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-dark bg-primary">
 <img src="includes/img/graphs_46879.png" alt="" width="50px" >
  <h2 class="Titulo">Calculo de estatistica</h2>
+ <a href="https://github.com/thyerre/calculo_estatistica" target="_blank"><img src="includes/img/github.svg" alt="" width="50px" >
 </nav>
